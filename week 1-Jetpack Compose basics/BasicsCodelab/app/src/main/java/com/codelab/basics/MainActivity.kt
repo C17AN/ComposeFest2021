6435fa5260490abfe3a8b4aadf58383f496ada5e
c17an <kimchanmin1@gmail.com>
2021-11-08 22:15:46 +0900

    feat: use new features: Row, Column, OutlinedButton

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,8 +3,12 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,30 +23,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                Greeting(name = "chanmin")
+                MyApp()
+//                Text("hi")
             }
         }
     }
 }
 
-@Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
-    }
-}
+//@Composable
+//private fun MyApp() {
+//    Surface(color = MaterialTheme.colors.background) {
+//        Greeting("Android")
+//    }
+//}
 
 @Composable
 private fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello, $name!", modifier=Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            Row() {
+                Text(text = "Hello, $name", modifier = Modifier.weight(1.0F))
+                OutlinedButton(onClick = { /*TODO*/ }) {
+                    Text("Show More")
+                }
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-private fun DefaultPreview() {
-    BasicsCodelabTheme() {
-        MyApp()
+private fun MyApp(nameList: List<String> = listOf("Chanmin", "David")) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            for (name in nameList) {
+                Greeting(name)
+            }
+        }
     }
 }
